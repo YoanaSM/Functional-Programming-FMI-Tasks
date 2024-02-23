653f8a1cb1c1df5dc2666a7d876ef5cb1178148b
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2024-02-23 14:30:05 +0200

    Add files via upload

---
+++ Week 01/Task4.hs
@@ -0,0 +1,11 @@
+main :: IO()
+main = do
+
+print (fibbIter 5)
+
+fibbIter :: Int -> Integer
+
+fibbIter x = helper 0 1 0
+  where
+    helper prev cur i =
+      if i == x then cur else helper cur (prev + cur) (i + 1)
