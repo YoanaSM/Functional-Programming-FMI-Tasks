56d85fda781a72264f00e53d74500dd0eedf7fac
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2024-04-05 12:26:46 +0300

    Add files via upload

---
+++ Week04/Task2GeneratePrimes.hs
@@ -0,0 +1,17 @@
+main::IO()
+main = do
+
+ print (generatePrimes 1 7)
+
+isPrime :: Int -> Bool
+isPrime 1 = False
+isPrime n = helper 2
+ where
+    helper d 
+        | d == n         = True
+        | mod n d == 0 = False
+        |otherwise       = helper (d + 1)
+   
+
+generatePrimes :: Int -> Int -> [Int]
+generatePrimes a b = [n | n <- [a..b], isPrime n ]
